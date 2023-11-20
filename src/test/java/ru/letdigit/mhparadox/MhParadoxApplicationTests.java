eb95ab6706399c28fc277d3c0d169f1ac5e1ed96
Eugene Elantsev <blinktreeman@gmail.com>
2023-11-20 20:56:29 +0900

    initial commit

---
+++ src/test/java/ru/letdigit/mhparadox/MhParadoxApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.letdigit.mhparadox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MhParadoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
